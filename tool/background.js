7e9ccb59785cf38166b245fc1b553e667e1cf8a0
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-11-30 15:39:14 +0800

    Create background.js

--- tool/background.js
+++ tool/background.js
@@ -19,7 +19,7 @@ var randomBgIndex = Math.round( Math.random() * 5 );
 var img=bodyBgs[randomBgIndex]
 console.log('背景图为：'+img)
 //输出随机的背景图
-if(img="undefined"){
+if(img=="undefined"){
   document.write('<style>body{background:url(' + "\\./tool/background/undefined.jpg" + ')no-repeat; background-size:cover;-webkit-background-attachment: fixed;background-attachment: fixed;}</style>');
 }
 else{
