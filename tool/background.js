2de15f1b75d2990f867c6a1b5a9a62556fb21807
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-11-30 15:03:12 +0800

    Update background.js

--- tool/background.js
+++ tool/background.js
@@ -1,19 +1,19 @@
 //<!CDATA[
 var bodyBgs = [];
-bodyBgs[0] = "\\./tool/background/1.jpg";
-bodyBgs[1] = "\\./tool/background/2.jpg";
-bodyBgs[2] = "\\./tool/background/3.jpg";
-bodyBgs[3] = "\\./tool/background/4.jpg";
-bodyBgs[4] = "\\./tool/background/5.jpg";
-//bodyBgs[5] = "\\./tool/background/6.jpg";
-//bodyBgs[6] = "\\./tool/background/7.jpg";
-//bodyBgs[7] = "\\./tool/background/8.jpg";
-//bodyBgs[8] = "\\./tool/background/9.jpg";
-//bodyBgs[9] = "\\./tool/background/10.jpg";
-//bodyBgs[10] = "\\./tool/background/11.jpg";
-//bodyBgs[11] = "\\./tool/background/12.jpg";
-//bodyBgs[12] = "\\./tool/background/13.jpg";
-//bodyBgs[13] = "\\./tool/background/14.jpg";
+bodyBgs[1] = "\\./tool/background/1.jpg";
+bodyBgs[2] = "\\./tool/background/2.jpg";
+bodyBgs[3] = "\\./tool/background/3.jpg";
+bodyBgs[4] = "\\./tool/background/4.jpg";
+bodyBgs[5] = "\\./tool/background/5.jpg";
+//bodyBgs[6] = "\\./tool/background/6.jpg";
+//bodyBgs[7] = "\\./tool/background/7.jpg";
+//bodyBgs[8] = "\\./tool/background/8.jpg";
+//bodyBgs[9] = "\\./tool/background/9.jpg";
+//bodyBgs[10] = "\\./tool/background/10.jpg";
+//bodyBgs[11] = "\\./tool/background/11.jpg";
+//bodyBgs[12] = "\\./tool/background/12.jpg";
+//bodyBgs[13] = "\\./tool/background/13.jpg";
+//bodyBgs[14] = "\\./tool/background/14.jpg";
 
 var randomBgIndex = Math.round( Math.random() * 5 );
 
