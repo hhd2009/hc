ebf15ee493600f9e6139ef09393b2becffa93dd0
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-12-01 15:51:43 +0800

    Update background.js

--- tool/background.js
+++ tool/background.js
@@ -1,26 +1,27 @@
 //<!CDATA[
+var dir="https://hhd2009.gthub.io/hc"
 var bodyBgs = [];
-bodyBgs[1] = "\\./tool/background/1.jpg";
-bodyBgs[2] = "\\./tool/background/2.jpg";
-bodyBgs[3] = "\\./tool/background/3.jpg";
-bodyBgs[4] = "\\./tool/background/4.jpg";
-bodyBgs[5] = "\\./tool/background/5.jpg";
-//bodyBgs[6] = "\\./tool/background/6.jpg";
-//bodyBgs[7] = "\\./tool/background/7.jpg";
-//bodyBgs[8] = "\\./tool/background/8.jpg";
-//bodyBgs[9] = "\\./tool/background/9.jpg";
-//bodyBgs[10] = "\\./tool/background/10.jpg";
-//bodyBgs[11] = "\\./tool/background/11.jpg";
-//bodyBgs[12] = "\\./tool/background/12.jpg";
-//bodyBgs[13] = "\\./tool/background/13.jpg";
-//bodyBgs[14] = "\\./tool/background/14.jpg";
+bodyBgs[1] = dir+"/tool/background/1.jpg";
+bodyBgs[2] = dir+"/tool/background/2.jpg";
+bodyBgs[3] = dir+"/tool/background/3.jpg";
+bodyBgs[4] = dir+"/tool/background/4.jpg";
+bodyBgs[5] = dir+"/tool/background/5.jpg";
+//bodyBgs[6] = dir+"/tool/background/6.jpg";
+//bodyBgs[7] = dir+"/tool/background/7.jpg";
+//bodyBgs[8] = dir+"/tool/background/8.jpg";
+//bodyBgs[9] = dir+"/tool/background/9.jpg";
+//bodyBgs[10] = dir+"/tool/background/10.jpg";
+//bodyBgs[11] = dir+"/tool/background/11.jpg";
+//bodyBgs[12] = dir+"/tool/background/12.jpg";
+//bodyBgs[13] = dir+"/tool/background/13.jpg";
+//bodyBgs[14] = dir+"/tool/background/14.jpg";
 
 var randomBgIndex = Math.round( Math.random() * 5 );
 var img=bodyBgs[randomBgIndex]
 console.log('背景图为：'+img)
 //输出随机的背景图
 if(img==undefined){
-  img="\\./tool/background/undefined.jpg"
+  img=dir+"/tool/background/undefined.jpg"
 }
 document.write(`
 <style>
