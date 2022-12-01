f9333030b638592be9fb8b48e25c5ee81bb6b613
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-12-01 16:04:12 +0800

    Update background.js

--- tool/background.js
+++ tool/background.js
@@ -1,20 +1,20 @@
 //<!CDATA[
 var dir="https://hhd2009.github.io/hc"
 var bodyBgs = [];
-bodyBgs[1] = dir+"/tool/background/1.jpg";
-bodyBgs[2] = dir+"/tool/background/2.jpg";
-bodyBgs[3] = dir+"/tool/background/3.jpg";
-bodyBgs[4] = dir+"/tool/background/4.jpg";
-bodyBgs[5] = dir+"/tool/background/5.jpg";
-//bodyBgs[6] = dir+"/tool/background/6.jpg";
-//bodyBgs[7] = dir+"/tool/background/7.jpg";
-//bodyBgs[8] = dir+"/tool/background/8.jpg";
-//bodyBgs[9] = dir+"/tool/background/9.jpg";
-//bodyBgs[10] = dir+"/tool/background/10.jpg";
-//bodyBgs[11] = dir+"/tool/background/11.jpg";
-//bodyBgs[12] = dir+"/tool/background/12.jpg";
-//bodyBgs[13] = dir+"/tool/background/13.jpg";
-//bodyBgs[14] = dir+"/tool/background/14.jpg";
+bodyBgs[1] = "/tool/background/1.jpg";
+bodyBgs[2] = "/tool/background/2.jpg";
+bodyBgs[3] = "/tool/background/3.jpg";
+bodyBgs[4] = "/tool/background/4.jpg";
+bodyBgs[5] = "/tool/background/5.jpg";
+//bodyBgs[6] = "/tool/background/6.jpg";
+//bodyBgs[7] = "/tool/background/7.jpg";
+//bodyBgs[8] = "/tool/background/8.jpg";
+//bodyBgs[9] = "/tool/background/9.jpg";
+//bodyBgs[10] = "/tool/background/10.jpg";
+//bodyBgs[11] = "/tool/background/11.jpg";
+//bodyBgs[12] = "/tool/background/12.jpg";
+//bodyBgs[13] = "/tool/background/13.jpg";
+//bodyBgs[14] = "/tool/background/14.jpg";
 
 var randomBgIndex = Math.round( Math.random() * 5 );
 var img=bodyBgs[randomBgIndex]
@@ -28,7 +28,7 @@ document.write(`
 body {
   font-family: "微软雅黑", sans-serif;
   /* 这里修改背景图 */
-  background: url(\'`+img+`\') no-repeat center;
+  background: url(\'`+dir+img+`\') no-repeat center;
   background-size: cover;
   -webkit-background-attachment: fixed;
   background-attachment: fixed;
