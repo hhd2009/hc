f132ccde4cb694e9f958a50ca4edde182de7c99f
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-12-03 11:37:46 +0800

    Update new.js

--- ad/new.js
+++ ad/new.js
@@ -1,14 +1,8 @@
 function new_ad(div,ad){
   document.getElementById(div).innerHTML=(`
 <!-- ad中包含【海报】和【关闭键】两部分 -->
-<script type="text/javascript">
-//关闭广告
-function close_ad(){
-  var ad = document.getElementById(\"${div}\");
-  ad.style.display = "none";
-}
-</script>
-<link rel="stylesheet" href="\./ad/style.css">
+<script type="text/javascript" src="\\./hc/ad/close.js"></script>
+<link rel="stylesheet" href="\\./hc/ad/style.css">
 <div id="ad">
   <!-- 海报 -->
   <div id="post">
@@ -16,7 +10,7 @@ function close_ad(){
   </div>
   <!-- 关闭键 -->
   <div id="close">
-    <a href="javascript:close_ad()"><img src="\./ad/close.jpg"></a>
+    <a href="javascript:close_ad(\"+${div}+\")"><img src="\\./hc/ad/close.jpg"></a>
   </div>      
 </div>`)
 }
