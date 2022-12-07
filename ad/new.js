05caa4f2f387318f80fbe997659b4464ddbbb8bc
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-12-07 10:58:11 +0800

    Update new.js

--- ad/new.js
+++ ad/new.js
@@ -3,7 +3,6 @@ var close_img="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAMCAgMCAg
 function new_ad(div,ad){
   document.getElementById(div).innerHTML=(`
 <!-- ad中包含【海报】和【关闭键】两部分 -->
-<script type="text/javascript" src="\\./hc/ad/close.js"></script>
 <link rel="stylesheet" href="\\./hc/ad/style.css">
 <div id="ad">
   <!-- 海报 -->
